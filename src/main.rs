e74eca5ce7180e904cf00222c192104692a544d8
Erik Näslund <contact@eriknaslund.com>
2024-01-06 19:51:43 +0100

    Read lines

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,34 @@ use clap::Parser;
 struct Args {
     #[arg(short, long, value_name = "path", help = "Print the byte count")]
     count: Option<std::path::PathBuf>,
+    #[arg(short, long, value_name = "path", help = "Print the line count")]
+    lines: Option<std::path::PathBuf>,
 }
 fn main() {
     let args = Args::parse();
     if let Some(c) = args.count {
-        println!("Counting bytes in {:?}", c.as_path());
-        let count = get_byte_count(&c).unwrap();
+        let count = read_bytes(&c).unwrap();
         println!("{} bytes", count);
+    }
+    if let Some(l) = args.lines {
+        let lines = count_lines(&l).unwrap();
+        println!("{} lines", lines);
     }
 }
 
-fn get_byte_count(path: &Path) -> std::io::Result<usize> {
+fn read_bytes(path: &Path) -> std::io::Result<usize> {
     let mut file = File::open(path)?;
     let mut buffer = Vec::new();
     let sum: usize = file.read_to_end(&mut buffer).iter().sum();
 
     Ok(sum)
+}
+
+fn count_lines(path: &Path) -> std::io::Result<usize> {
+    let mut file = File::open(path)?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+    let lines: Vec<String> = buffer.lines().map(|s| s.to_string()).collect();
+
+    Ok(lines.len())
 }
