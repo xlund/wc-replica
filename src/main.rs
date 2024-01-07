44a23973f6e3d0fea45195d125150ab85032398c
Erik Näslund <contact@eriknaslund.com>
2024-01-07 02:04:24 +0100

    Handle io streams

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{
+    fmt::{self, Display, Formatter},
     fs::File,
     io::{self, Read},
+    path,
 };
 
 use clap::Parser;
@@ -8,7 +10,7 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 
 struct Args {
-    file: std::path::PathBuf,
+    file: Option<std::path::PathBuf>,
     #[arg(short, help = "Print byte count", group = "count")]
     c_byte: bool,
     #[arg(short, help = "Print line count", group = "count")]
@@ -19,61 +21,95 @@ struct Args {
     m_char: bool,
 }
 
-enum Target {
-    Byte,
-    Line,
-    Word,
-    Char,
-    None,
+#[derive(Debug)]
+enum Path {
+    Stdin,
+    File(std::path::PathBuf),
 }
-impl Target {
-    pub fn new(args: &Args) -> Target {
-        if args.c_byte {
-            Target::Byte
-        } else if args.l_line {
-            Target::Line
-        } else if args.w_word {
-            Target::Word
-        } else if args.m_char {
-            Target::Char
-        } else {
-            Target::None
+impl Display for Path {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Path::Stdin => write!(f, "stdin"),
+            Path::File(path) => write!(f, "{}", path.display()),
         }
     }
-    pub fn from_buffer(&self, buffer: String) -> String {
-        match self {
-            Target::Byte => buffer.len().to_string(),
-            Target::Line => buffer.lines().count().to_string(),
-            Target::Word => buffer.split_whitespace().count().to_string(),
-            Target::Char => buffer.chars().count().to_string(),
-            Target::None => {
+}
+#[derive(Debug)]
+struct Ccwc {
+    path: Path,
+    buffer: String,
+    target: Target,
+}
+impl Ccwc {
+    fn from_args(args: Args) -> Result<Ccwc, io::Error> {
+        let path = match &args.file {
+            Some(path) => Path::File(path.clone()),
+            None => Path::Stdin,
+        };
+        let buffer = Self::path_to_buffer(&path)?;
+        let target = Target::from_args(buffer.clone(), args);
+
+        Ok(Ccwc {
+            path,
+            buffer,
+            target,
+        })
+    }
+
+    fn path_to_buffer(path: &Path) -> Result<String, io::Error> {
+        let mut buffer = String::new();
+        match path {
+            Path::Stdin => {
+                let _ = io::stdin().read_to_string(&mut buffer)?;
+                Ok(buffer)
+            }
+            Path::File(path) => {
+                let mut file = File::open(path)?;
+                let _ = file.read_to_string(&mut buffer)?;
+                Ok(buffer)
+            }
+        }
+    }
+}
+#[derive(Debug)]
+enum Target {
+    Byte(usize),
+    Line(usize),
+    Word(usize),
+    Char(usize),
+    None(usize, usize, usize),
+}
+impl Target {
+    pub fn from_args(buffer: String, args: Args) -> Target {
+        match args {
+            Args { c_byte: true, .. } => Target::Byte(buffer.len()),
+            Args { l_line: true, .. } => Target::Line(buffer.lines().count()),
+            Args { w_word: true, .. } => Target::Word(buffer.split_whitespace().count()),
+            Args { m_char: true, .. } => Target::Char(buffer.chars().count()),
+            Args { .. } => {
                 let lines = buffer.lines().count();
                 let words = buffer.split_whitespace().count();
-                let bytes = buffer.len().to_string();
-                format!("{} {} {}", lines, words, bytes)
+                let bytes = buffer.len();
+                Target::None(lines, words, bytes)
             }
+        }
+    }
+}
+impl Display for Target {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Target::Byte(byte) => write!(f, "{}", byte),
+            Target::Line(line) => write!(f, "{}", line),
+            Target::Word(word) => write!(f, "{}", word),
+            Target::Char(char) => write!(f, "{}", char),
+            Target::None(line, word, byte) => write!(f, "{} {} {}", line, word, byte),
         }
     }
 }
 
 fn main() {
     let args = Args::parse();
-    let path = &args.file;
-    let file = File::open(path).expect("Unable to open file");
-    let _ = run(args, file);
-}
+    let ccwc = Ccwc::from_args(args).unwrap();
 
-fn read_file(mut file: File) -> Result<String, io::Error> {
-    let mut buffer = String::new();
-    let _ = file.read_to_string(&mut buffer)?;
-    Ok(buffer)
-}
-
-fn run(args: Args, file: File) -> Result<(), io::Error> {
-    let target = Target::new(&args);
-    let buffer = read_file(file)?;
-    let path = &args.file.display();
-    let count = target.from_buffer(buffer);
-    println!("{} {}", count, path);
-    Ok(())
+    println!("{} {}", ccwc.target, ccwc.path);
 }
